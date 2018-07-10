c997734dc52cda0f9106af26630b11730efe4952
rath-san <rim.kozlowski@gmail.com>
2018-07-10 12:40:13 +0200

    container max-width on homepage

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../../_services/main.service';
 import { Observable } from 'rxjs';
+import { Movie } from '../../_models/movie';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,18 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  movies: Observable<any>;
+  movies: Observable<Movie[]>;
   resultsCount: Observable<number>;
+  resultsPages: Observable<number>;
+  searchQuery: Observable<string>;
 
   constructor(
     private _mainService: MainService
   ) {
     this.movies = this._mainService.$movies;
     this.resultsCount = this._mainService.$resultsCount;
+    this.resultsPages = this._mainService.$pagesCount;
+    this.searchQuery = this._mainService.$searchQuery;
   }
 
   ngOnInit() {
