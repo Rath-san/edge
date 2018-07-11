2b2e98c6326ee1ef7a5ffe96ca0e801b8ddfd9de
rath-san <rim.kozlowski@gmail.com>
2018-07-11 11:15:26 +0200

    pagination styles

--- src/app/_components/pagination/pagination.component.ts
+++ src/app/_components/pagination/pagination.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../../_services/main.service';
+import { of, Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-pagination',
@@ -14,56 +15,77 @@ export class PaginationComponent implements OnInit {
   firstPageDeactivated: boolean;
   lastPageDeactivated: boolean;
 
+  visiblePages: BehaviorSubject<number[]> = new BehaviorSubject<number[]>(null);
+
   constructor(
     private _mainService: MainService
   ) {
-    this.pagesCount = this._mainService.$pagesCount;
+    this._mainService.$pagesCount.subscribe(y => {
+      this.pagesCount = y;
+    });    
     this.activePage = this._mainService.$activePage.subscribe(x => {
       this._mainService.getMovies(this._mainService.$searchQuery.getValue(), x);
-      if(x === 1) {
+      if (x === 1) {
         this.firstPageDeactivated = true
       } else {
         this.firstPageDeactivated = false
       }
 
-      if(x === this.pagesCount.getValue()) {
+      if (x === this.pagesCount) {
         this.lastPageDeactivated = true;
       } else {
         this.lastPageDeactivated = false;
       }
-    })
-    
+      this.populatePagination(x);
+    });
+
   }
 
   ngOnInit() {
   }
 
-  goPage(page: number) {
-    console.log(`go to ${page}`);
+  public populatePagination(pageNumber: number) {  
+    console.log(pageNumber);
+    console.log(this.pagesCount);
+    
+      
+    const currentPage = pageNumber;
+    let visiblePages = [];
+    for (let index = currentPage - 3; index < currentPage + 3; index++) {      
+      if (index > 0 && index <= this.pagesCount) {
+        visiblePages.push(index);
+      }
+      this.visiblePages.next(visiblePages);
+    }
   }
+    goPage(page: number) {
+      console.log(`go to ${page}`);
+      this._mainService.$activePage.next(page);
+    }
+
+    goFirst() {
+      console.log('first');
+      this._mainService.$activePage.next(1);
+    }
+
+    goPrev() {
+      let page = this._mainService.$activePage.getValue();
+      this._mainService.$activePage.next(page - 1);
+    }
+
+    goLast() {
+      console.log('last');
+      this._mainService.$activePage.next(this.pagesCount);
+    }
+
+    goNext() {
+      let page = this._mainService.$activePage.getValue();
+
+      this._mainService.$activePage.next(page + 1);
+    }
 
 
-  goFirst() {
-    console.log('first');
-    this._mainService.$activePage.next(1);
+
+
+
   }
-
-  goLast() {
-    console.log('last');
-    this._mainService.$activePage.next(this.pagesCount.getValue());
-  }
-
-  goNext() {
-    let page = this._mainService.$activePage.getValue();
-
-    this._mainService.$activePage.next(page + 1);
-  }
-
-  goPrev() {
-    let page = this._mainService.$activePage.getValue();
-    this._mainService.$activePage.next(page - 1);
-  }
-
-
-
-}
