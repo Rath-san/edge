a2b06e1f1cc33ddfcde60c2ec4da849802e911c0
rath-san <rim.kozlowski@gmail.com>
2018-07-12 20:49:52 +0200

    pagination go to functionality and states

--- src/app/_components/pagination/pagination.component.ts
+++ src/app/_components/pagination/pagination.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MainService } from '../../_services/main.service';
-import { of, Observable, BehaviorSubject } from 'rxjs';
-
+import { of, Observable, BehaviorSubject, forkJoin, combineLatest } from 'rxjs';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
 
   activePage
   pagesCount
@@ -15,69 +15,97 @@ export class PaginationComponent implements OnInit {
   firstPageDeactivated: boolean;
   lastPageDeactivated: boolean;
 
+  pageInputOpen: boolean = false;
+
   visiblePages: BehaviorSubject<number[]> = new BehaviorSubject<number[]>(null);
 
   constructor(
     private _mainService: MainService
   ) {
-    this._mainService.$pagesCount.subscribe(y => {
-      this.pagesCount = y;
-    });    
-    this.activePage = this._mainService.$activePage.subscribe(x => {
-      this._mainService.getMovies(this._mainService.$searchQuery.getValue(), x);
-      if (x === 1) {
-        this.firstPageDeactivated = true
-      } else {
-        this.firstPageDeactivated = false
-      }
+    combineLatest(
+      this._mainService.$pagesCount,
+      this._mainService.$activePage
+    )
+      .subscribe(([y, x]) => {
+        this.pagesCount = y;
+        this.activePage = x;
+        if (x === 1) {
+          this.firstPageDeactivated = true
+        } else {
+          this.firstPageDeactivated = false
+        }
 
-      if (x === this.pagesCount) {
-        this.lastPageDeactivated = true;
-      } else {
-        this.lastPageDeactivated = false;
-      }
-      this.populatePagination(x);
-    });
+        if (x === this.pagesCount) {
+          this.lastPageDeactivated = true;
+        } else {
+          this.lastPageDeactivated = false;
+        }        
+        this.populatePagination(x);
+      });
 
   }
 
-  ngOnInit() {
-  }
-
-  public populatePagination(pageNumber: number) {  
+  public populatePagination(pageNumber: number): void {
     const currentPage = pageNumber;
     let visiblePages = [];
-    for (let index = currentPage - 3; index < currentPage + 3; index++) {      
+    for (let index = currentPage - 3; index < currentPage + 3; index++) {
       if (index > 0 && index <= this.pagesCount) {
         visiblePages.push(index);
       }
       this.visiblePages.next(visiblePages);
     }
   }
-    goPage(page: number) {
-      this._mainService.$activePage.next(page);
-    }
-
-    goFirst() {
-      this._mainService.$activePage.next(1);
-    }
-
-    goPrev() {
-      let page = this._mainService.$activePage.getValue();
-      this._mainService.$activePage.next(page - 1);
-    }
-
-    goLast() {
-      this._mainService.$activePage.next(this.pagesCount);
-    }
-
-    goNext() {
-      let page = this._mainService.$activePage.getValue();
-      this._mainService.$activePage.next(page + 1);
-    }
-
-
-
-
-
+  goPage(page: number): void {
+    this._mainService.$activePage.next(page);
+    this._search();
   }
+
+  goFirst(): void {
+    this._mainService.$activePage.next(1);
+    this._search();
+  }
+
+  goPrev(): void {
+    let page = this._mainService.$activePage.getValue();
+    this._mainService.$activePage.next(page - 1);
+    this._search();
+  }
+
+  goLast(): void {
+    this._mainService.$activePage.next(this.pagesCount);
+    this._search();
+  }
+
+  goNext(): void {
+    let page = this._mainService.$activePage.getValue();
+    this._mainService.$activePage.next(page + 1);
+    this._search();
+  }
+
+  public openPageInput(): void {
+    this.pageInputOpen = !this.pageInputOpen;
+  }
+
+  public gotoSpecificPageOnKeyUp(value: number, event?: KeyboardEvent): void {
+    if (event.keyCode == 13) {
+      this._mainService.$activePage.next(Number(value));
+      this._search();
+      this.openPageInput();
+    }
+  }
+  public gotoSpecificPageOnBlur(value: number): void {
+      this._mainService.$activePage.next(Number(value));
+      this._search();
+      this.openPageInput();
+  }
+
+  private _search(): void {
+    this._mainService.getMovies(this._mainService.$searchQuery.getValue(), this.activePage);
+  }
+
+
+
+
+
+
+}
